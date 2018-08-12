855b98f7cf3b1325988ddeedb82f6e0d1c405f6e
AlexRim <Squire_86@mail.ru>
2018-08-12 19:57:18 +0300

    add few methods

--- AllStarsSolution/AllStarsSolution/DriverExtensions/DriverExtension.cs
+++ AllStarsSolution/AllStarsSolution/DriverExtensions/DriverExtension.cs
@@ -8,15 +8,10 @@ namespace AllStarsSolution.DriverExtensions
 {
     public static class DriverExtension
     {
-        public static void WaitForPageLoad()
-        {
-            Browser.GetDriver().Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(Browser.timeOut);
-        }
+      public static void Click(IWebElement element) =>Browser.Driver.InvokeAction(action => element.Click());
 
-        public static IWebElement GetElement(By locator)
-        {
-           return Browser.GetDriver().FindElement(locator);
-        }
+      public static IWebElement GetElement(By locator) =>Browser.Driver.InvokeFunc<IWebElement>(element => element.FindElement(locator));
+
 
 
     }
---
+++ AllStarsSolution/AllStarsSolution/DriverExtensions/ElementExtension.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllStarsSolution.DriverExtensions
+{
+    public static  class ElementExtension
+    {
+        public static IWebElement GetElement(IWebDriver driver, By locator) => driver.FindElement(locator);
+
+        public static IReadOnlyCollection<IWebElement> GetElements(IWebDriver driver, By locator) => driver.FindElements(locator);
+
+
+    }
+}
