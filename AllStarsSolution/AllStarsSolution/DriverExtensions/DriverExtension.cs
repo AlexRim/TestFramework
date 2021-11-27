1b64fce9f2c0db5127a3e850ba6d0ec38a0085a1
Aliaksandr_Rymasheuski <Aliaksandr_Rymasheuski1@epam.com>
2021-11-27 15:56:40 +0300

    Refactoring

--- AllStarsSolution/AllStarsSolution/DriverExtensions/DriverExtension.cs
+++ AllStarsSolution/AllStarsSolution/DriverExtensions/DriverExtension.cs
@@ -11,6 +11,5 @@ namespace AllStarsSolution.DriverExtensions
         public static void Click(IWebElement element) => Browser.InvokeAction(action => element.Click());
 
         public static IWebElement GetElement(By locator) => Browser.InvokeFunc<IWebElement>(element => element.FindElement(locator));
-
     }
 }
